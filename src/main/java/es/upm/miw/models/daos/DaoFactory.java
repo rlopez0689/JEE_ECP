9c9022d90671726a7bf301b24d2de5b7cb3fff9a
rodrigolopez <rodrigo@wime.com.mx>
2015-03-04 13:39:57 +0100

    Add DaoFactory class --Dependency log4 added

---
+++ src/main/java/es/upm/miw/models/daos/DaoFactory.java
@@ -0,0 +1,22 @@
+package es.upm.miw.models.daos;
+
+import es.upm.miw.models.daos.ThemeDao;
+import es.upm.miw.models.daos.VoteDao;
+
+public abstract class DaoFactory {
+    public static DaoFactory factory = null;
+
+    public static void setFactory(DaoFactory factory) {
+        DaoFactory.factory = factory;
+    }
+
+    public static DaoFactory getFactory() {
+        assert factory != null;
+        return factory;
+    }
+
+    public abstract ThemeDao getUserDao();
+
+    public abstract VoteDao getCategoryDao();
+
+}
