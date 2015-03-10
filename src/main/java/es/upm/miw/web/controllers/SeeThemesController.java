e5c9a090492f60d8770cdb63fa5769704c91a63a
rodrigolopez <rodrigo@wime.com.mx>
2015-03-10 09:28:06 +0100

    Initial change of web architecture.

--- src/main/java/es/upm/miw/web/controllers/SeeThemesController.java
+++
@@ -1,5 +0,0 @@
-package es.upm.miw.web.controllers;
-
-public class SeeThemesController {
-
-}
--- src/main/java/es/upm/miw/web/controllers/ejbs/SeeThemesControllerEJB.java
+++
@@ -1,5 +0,0 @@
-package es.upm.miw.web.controllers.ejbs;
-
-public class SeeThemesControllerEJB {
-
-}
