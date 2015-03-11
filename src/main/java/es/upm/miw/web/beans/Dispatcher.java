110b764f7f780bdd406eff9cfd7dd94adfc725fa
rodrigolopez <rodrigo@wime.com.mx>
2015-03-11 21:39:58 +0100

    Add Index & Home View for JSP

--- src/main/java/es/upm/miw/web/beans/Dispatcher.java
+++ src/main/java/es/upm/miw/web/beans/Dispatcher.java
@@ -1,5 +1,45 @@
 package es.upm.miw.web.beans;
 
-public class Dispatcher {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/v1/*")
+public class Dispatcher extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private static String PATH_ROOT_VIEW = "/votesJSP/";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String action = request.getPathInfo().substring(1);
+
+        String view;
+        switch (action) {
+        case "temas":
+            SeeThemesView themesView = new SeeThemesView();
+            //personaView.setPersona(new Persona());
+            request.setAttribute(action, themesView);
+            view = action;
+            break;
+        case "rol":
+            SeeVotesView rolView = new SeeVotesView();
+            request.setAttribute(action, rolView);
+            view = action;
+            break;
+        default:
+            view = "home";
+        }
+
+        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+                .forward(request, response);
+
+    }
 }
---
+++ src/main/webapp/votesJSP/home.jsp
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"  ?>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Home Votaciones</title>
+</head>
+<body>
+	<h1>Home Votaciones: V1</h1>
+	<p>
+		<a href="/Web/v1/#">Agregar Tema</a>
+	</p>
+	<p>
+		<a href="/Web/v1/#">Borrar Tema</a>
+	</p>
+	<p>
+		<a href="/Web/v1/#">Votar</a>
+	</p>
+	<p>
+		<a href="/Web/v1/#">Ver Votaciones</a>
+	</p>
+</body>
+</html>
