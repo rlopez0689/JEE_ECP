1534afc60d2f6380045c01b50daa06c4654cab64
rodrigolopez <rodrigo@wime.com.mx>
2015-03-20 22:38:56 +0100

    Add URIS classes for theme & votes

---
+++ src/main/java/es/upm/miw/ws/ThemeUris.java
@@ -0,0 +1,6 @@
+package es.upm.miw.ws;
+
+public interface ThemeUris {
+	String PATH_THEMES = "/themes";
+	String PATH_VOTES = "/{id}";
+}
---
+++ src/main/java/es/upm/miw/ws/VoteUris.java
@@ -0,0 +1,6 @@
+package es.upm.miw.ws;
+
+public interface VoteUris {
+	String PATH_VOTACIONES="/votaciones";
+	String PATH_VOTAR="/votar";
+}
