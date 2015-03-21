4849f88f09da3ca4583a2656922cd1c134461105
rodrigolopez <rodrigo@wime.com.mx>
2015-03-21 21:20:31 +0100

    Add ControllerWsFactory class

---
+++ src/main/java/es/upm/miw/controllers/ws/ControllerWsFactory.java
@@ -0,0 +1,42 @@
+package es.upm.miw.controllers.ws;
+
+import es.upm.miw.web.controllers.AddThemeController;
+import es.upm.miw.web.controllers.ControllerFactory;
+import es.upm.miw.web.controllers.DeleteThemeController;
+import es.upm.miw.web.controllers.SeeVotesController;
+import es.upm.miw.web.controllers.ThemeController;
+import es.upm.miw.web.controllers.VoteController;
+
+public class ControllerWsFactory extends ControllerFactory {
+
+	@Override
+	public AddThemeController getAddThemeController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DeleteThemeController getDeleteThemeController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SeeVotesController getSeeVotesController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VoteController getVoteController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ThemeController getThemeController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
