d20bb63bbf89e948b62e3974fa9aeae71488f234
rodrigolopez <rodrigo@wime.com.mx>
2015-03-23 19:13:42 +0100

    Add VoteEntityTest class for testing equals vote

--- src/test/java/es/upm/miw/persitence/models/entities/VoteEntityTest.java
+++ src/test/java/es/upm/miw/persitence/models/entities/VoteEntityTest.java
@@ -4,11 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import es.upm.miw.persistence.models.entities.Theme;
+import es.upm.miw.persistence.models.entities.Vote;
+import es.upm.miw.persistence.models.utils.EducationLevel;
+
 public class VoteEntityTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEquals() {
+		Theme t1 = new Theme(5,"prueba", "prueba");
+		Vote v1 = new Vote(5, "125.0.0.1", t1, 3, EducationLevel.ASSOCITEDEGREE);
+		Vote v2 = new Vote(5, "125.0.0.1", t1, 3, EducationLevel.ASSOCITEDEGREE);
+		Vote v3 = new Vote(6, "125.0.0.2", t1, 5, EducationLevel.DOCTORALDEGREE);	
+		assertTrue(v1.equals(v2));
+		assertFalse(v1.equals(v3));
 	}
 
 }
