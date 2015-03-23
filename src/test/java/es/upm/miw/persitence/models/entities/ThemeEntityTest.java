d7f9b68e285f5c27649493e5b16e5c63c42751cd
rodrigolopez <rodrigo@wime.com.mx>
2015-03-23 19:01:47 +0100

    Add ThemeEntityTest class for testing equals method of theme

--- src/test/java/es/upm/miw/persitence/models/entities/ThemeEntityTest.java
+++ src/test/java/es/upm/miw/persitence/models/entities/ThemeEntityTest.java
@@ -4,11 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import es.upm.miw.persistence.models.entities.Theme;
+
 public class ThemeEntityTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEquals() {
+		Theme t1 = new Theme(5,"prueba", "prueba");
+		Theme t2 = new Theme(5,"prueba", "prueba");
+		Theme t3 = new Theme(3,"prueba2", "prueba3");		
+		assertTrue(t1.equals(t2));
+		assertFalse(t1.equals(t3));
 	}
 
 }
